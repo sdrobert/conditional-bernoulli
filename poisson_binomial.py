9b8fdd6eabbee06c1f0e3f10c8e350d0105feab9
Sean Robertson <sdrobert@cs.toronto.edu>
2020-03-06 13:29:22 -0500

    Added shift_R, log_shift_R

--- poisson_binomial.py
+++ poisson_binomial.py
@@ -1,10 +1,11 @@
 import torch
 
 
+# this isn't numerically stable
 def naive_R(w, k_max):
     S, = w.shape
     assert 0 <= k_max
-    R = torch.tensor([1.])
+    R = torch.ones(1, device=w.device)
     T = (
         ((-w).unsqueeze(0) ** torch.arange(1, k_max + 2).unsqueeze(1))
         / w.unsqueeze(0)
@@ -16,3 +17,29 @@ def naive_R(w, k_max):
             R_new = torch.zeros(1)
         R = torch.cat([R, R_new], dim=0)
     return R
+
+
+def shift_R(w, k_max):
+    # in w = (T, *)
+    # out R = (k_max + 1, *)
+    R0 = torch.ones_like(w[:1, ...])
+    Rrest = torch.zeros((k_max,) + w[0].size(), device=w.device)
+    w = w.unsqueeze(1)
+    for T in range(w.shape[0]):
+        Rrest = Rrest + w[T] * torch.cat([R0, Rrest[:-1]], 0)
+    return torch.cat([R0, Rrest])
+
+
+def shift_log_R(theta, k_max):
+    # in theta = (T, *)
+    # out log_R = (k_max + 1, *)
+    log_R0 = torch.zeros_like(theta[:1, ...])
+    log_Rrest = torch.full(
+        (k_max,) + theta[0].size(), -float('inf'), device=theta.device)
+    theta = theta.unsqueeze(1)
+    for T in range(theta.shape[0]):
+        x = torch.cat([log_R0, log_Rrest[:-1]], 0)
+        x = torch.where(torch.isfinite(x), x + theta[T], x)
+        log_Rrest = torch.logsumexp(torch.stack([log_Rrest, x], 0), 0)
+        del x
+    return torch.cat([log_R0, log_Rrest])
--- test_poisson_binomial.py
+++ test_poisson_binomial.py
@@ -22,3 +22,74 @@ def test_naive_R():
     assert torch.allclose(R_exp, R_act, atol=1e-5)
     g_act, = torch.autograd.grad(R_act, w, torch.ones_like(R_act))
     assert torch.allclose(g_exp, g_act, atol=1e-5)
+
+
+def test_shift_R():
+    torch.manual_seed(2154)
+    w = torch.rand(50)
+    R_exp = poisson_binomial.naive_R(w, 10)
+    R_act = poisson_binomial.shift_R(w, 10)
+    assert torch.allclose(R_exp, R_act)
+    R_act = poisson_binomial.shift_R(w.unsqueeze(-1).expand(-1, 30), 10)
+    assert torch.allclose(R_exp, R_act[..., 11])
+
+
+def test_shift_R_zero_weights():
+    torch.manual_seed(1702)
+    T, N, k_max = 30, 10, 4
+    w_lens = torch.randint(k_max, T + 1, (N,))
+    w = []
+    R_exp = []
+    g_exp = []
+    for w_len in w_lens.tolist():
+        w_n = torch.rand(w_len).requires_grad_(True)
+        R_n = poisson_binomial.shift_R(w_n, k_max)
+        g_n, = torch.autograd.grad(R_n, w_n, torch.ones_like(R_n))
+        R_exp.append(R_n)
+        g_exp.append(g_n)
+        w.append(w_n)
+    w = torch.nn.utils.rnn.pad_sequence(w)
+    R_exp = torch.stack(R_exp, dim=-1)
+    w.requires_grad_(True)
+    R_act = poisson_binomial.shift_R(w, k_max)
+    assert torch.allclose(R_exp, R_act)
+    g_act, = torch.autograd.grad(R_act, w, torch.ones_like(R_act))
+    # zero weights can still have a gradient, but that shouldn't affect the
+    # gradient of other weights
+    for w_len, g_exp_n, g_act_n in zip(w_lens, g_exp, g_act.T):
+        assert torch.allclose(g_exp_n[:w_len], g_act_n[:w_len])
+
+
+def test_shift_log_R():
+    torch.manual_seed(198236)
+    w = torch.rand(50, 4, 30, 10)
+    R_exp = poisson_binomial.shift_R(w, 20)
+    R_act = poisson_binomial.shift_log_R(w.log(), 20).exp()
+    assert torch.allclose(R_exp, R_act)
+
+
+def test_shift_log_R_inf_theta():
+    torch.manual_seed(3291)
+    T, N, k_max = 53, 5, 3
+    theta_lens = torch.randint(k_max, T + 1, (N,))
+    theta = []
+    R_exp = []
+    g_exp = []
+    for theta_len in theta_lens.tolist():
+        theta_n = torch.randn(theta_len).requires_grad_(True)
+        R_n = poisson_binomial.shift_log_R(theta_n, k_max)
+        g_n, = torch.autograd.grad(R_n, theta_n, torch.ones_like(R_n))
+        R_exp.append(R_n)
+        g_exp.append(g_n)
+        theta.append(theta_n)
+    theta = torch.nn.utils.rnn.pad_sequence(theta, padding_value=-float('inf'))
+    R_exp = torch.stack(R_exp, dim=-1)
+    theta.requires_grad_(True)
+    R_act = poisson_binomial.shift_log_R(theta, k_max)
+    assert torch.allclose(R_exp, R_act)
+    g_act, = torch.autograd.grad(R_act, theta, torch.ones_like(R_act))
+    # zero weights can still have a gradient, but that shouldn't affect the
+    # gradient of other weights
+    for theta_len, g_exp_n, g_act_n in zip(theta_lens, g_exp, g_act.T):
+        assert torch.allclose(g_exp_n[:theta_len], g_act_n[:theta_len])
+        assert torch.all(torch.isfinite(g_act_n[theta_len:]))
