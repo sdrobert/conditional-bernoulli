0411c2d4d28dcdd5353ca5ccd6751e4bb605594f
Sean Robertson <sdrobert@cs.toronto.edu>
2020-04-21 17:31:23 -0400

    Added bounded_bernoulli.lsample, test

--- bounded_bernoulli.py
+++ bounded_bernoulli.py
@@ -2,6 +2,8 @@ import torch
 from poisson_binomial import R, lR
 
 
+# this is more of a sanity check than anything. It should generate samples
+# from the CB, but using the BB step function
 def sample(w, counts):
     # in w = (T, *), counts = (*) or int
     # out b = (counts.max(), *)
@@ -47,52 +49,49 @@ def sample(w, counts):
     b = torch.stack(b[1:], 0).view(max_count, *orig_shape[1:])
     return b
 
-# def sample(w, counts):
-#     # in w = (T, *), counts = (*) or int
-#     # out b = (counts.max(), *)
 
-#     w = w.detach()
-#     if not torch.is_tensor(counts):
-#         counts = torch.tensor(counts, device=w.device)
-#     counts = counts.expand_as(w[0]).detach().flatten()  # (N,)
+def lsample(logits, counts):
+    # in logits = (T, *), counts = (*) or int
+    # out b = (counts.max(), *)
 
-#     orig_shape = w.shape
-#     T = orig_shape[0]
-#     max_count = counts.max().item()
-#     assert 0 <= max_count <= T
-#     if not max_count:
-#         return torch.empty(0, *orig_shape[1:], device=w.device, dtype=int)
+    logits = logits.detach()
+    if not torch.is_tensor(counts):
+        counts = torch.tensor(counts, device=logits.device)
+    counts = counts.expand_as(logits[0]).detach().flatten()  # (N,)
 
-#     Rhist = R(w, max_count, True, True)  # (T + 1, max_count + 1, *)
-#     w = w.transpose(0, -1).flatten(end_dim=-2)  # (N, T)
-#     b = [torch.full_like(w[..., 0], -1, dtype=int)]  # [(N,)]
-#     Rhist = Rhist.flatten(2).transpose(0, 1).transpose(1, 2).flip(2)
-#     t = torch.arange(T, dtype=int, device=w.device)
-#     # Rhist[c, :, t] = R(c, A^c_{t + 1})
+    orig_shape = logits.shape
+    T = orig_shape[0]
+    max_count = counts.max().item()
+    assert 0 <= max_count <= T
+    if not max_count:
+        return torch.empty(0, *orig_shape[1:], device=logits.device, dtype=int)
 
-#     # P(t = t_l|L - l, t_{l-1}) = w_t R(L - l, A^c_t)
-#     #                             / R(L - l + 1, A^c_t_{l-1})
-#     #                           = w[:, t - 1] Rhist[L - l, :, t - 1] /
-#     #                             Rhist[L - l + 1, :, b[-1]]
-#     #                           = p[:, t - 1]
-#     for l in range(1, max_count + 1):
-#         Lml = counts - l
-#         R_Lml = Rhist.gather(
-#             0, Lml.clamp(0).unsqueeze(0).unsqueeze(-1).expand_as(Rhist[1:]))[0]
-#         t_lm1 = b[-1]
-#         p = (
-#             (w * R_Lml[:, 1:]).masked_fill((t_lm1.unsqueeze(-1) >= t), 0.)
-#             .masked_fill((Lml < 0).unsqueeze(-1), 1.)
-#         )
+    Rhist = lR(logits, max_count, True, True)  # (T + 1, max_count + 1, *)
+    logits = logits.transpose(0, -1).flatten(end_dim=-2)  # (N, T)
+    b = [torch.full_like(logits[..., 0], -1, dtype=int)]  # [(N,)]
+    Rhist = Rhist[:-1].flatten(2).transpose(0, 1).transpose(1, 2).flip(2)
+    t = torch.arange(T, dtype=int, device=logits.device)
+    # Rhist[c, :, t] = R(c, A^c_{t + 1})
 
-#         R_Lmlp1 = Rhist.gather(
-#             0, (Lml + 1).clamp(0).unsqueeze(0).unsqueeze(-1).expand_as(Rhist[1:]))[0]
-#         R_Lmlp1_tlm1 = R_Lmlp1.gather(1, (t_lm1 + 1).unsqueeze(-1))[..., 0]
+    # P(t = t_l|L - l, t_{l-1}) = w_t R(L - l, A^c_t)
+    #                             / R(L - l + 1, A^c_t_{l-1})
+    #                           = logits[:, t - 1] Rhist[L - l, :, t - 1] /
+    #                             Rhist[L - l + 1, :, b[-1]]
+    #                           = p[:, t - 1]
+    for l in range(1, max_count + 1):
+        Lml = counts - l
+        R_Lml = Rhist.gather(
+            0, Lml.clamp(0).unsqueeze(0).unsqueeze(-1).expand_as(Rhist[1:]))[0]
+        t_lm1 = b[-1]
 
-#         assert torch.all(torch.isclose(p.sum(1), R_Lmlp1_tlm1) | (Lml < 0))
+        lp = (
+            (logits + R_Lml)
+            .masked_fill((t_lm1.unsqueeze(-1) >= t), -float('inf'))
+            .masked_fill((Lml < 0).unsqueeze(-1), 0.)
+        )
 
-#         dist = torch.distributions.Categorical(probs=p)
-#         b.append(dist.sample().masked_fill(Lml < 0, -1))
+        dist = torch.distributions.Categorical(logits=lp)  # normalizes
+        b.append(dist.sample().masked_fill(Lml < 0, -1))
 
-#     b = torch.stack(b[1:], 0).view(max_count, *orig_shape[1:])
-#     return b
+    b = torch.stack(b[1:], 0).view(max_count, *orig_shape[1:])
+    return b
--- test_bounded_bernoulli.py
+++ test_bounded_bernoulli.py
@@ -13,11 +13,11 @@ def to_one_hot(b_index, len_, dim=0):
 
 
 def test_sample():
-    torch.manual_seed(40272)
-    T, N = 10, 100000
+    torch.manual_seed(9302)
+    T, N = 20, 100000
     w = torch.rand(T)
     w[::2] = 0.
-    counts = torch.tensor([4, 5]).unsqueeze(0).expand(N, 2)
+    counts = torch.tensor([9, 10]).unsqueeze(0).expand(N, 2)
 
     s = to_one_hot(bounded_bernoulli.sample(
         w.view(T, 1, 1).expand(T, N, 2), counts
@@ -32,3 +32,22 @@ def test_sample():
     props2 = s.mean(1)
 
     assert torch.allclose(props1, props2, atol=1e-2)
+
+
+def test_lsample():
+    torch.manual_seed(2318)
+    T, N = 105, 15
+    logits = torch.randn(T, N)
+    logits[::2] = -float('inf')
+    counts = torch.randint(1, T // 2 - 1, (N,))
+
+    torch.manual_seed(1)
+    b1 = bounded_bernoulli.sample(logits.exp(), counts)
+
+    torch.manual_seed(1)
+    b2 = bounded_bernoulli.lsample(logits, counts)
+
+    assert torch.all((b1 >= 0).sum(0).int() == counts)
+    assert torch.all((b2 >= 0).sum(0).int() == counts)
+
+    assert torch.all(b1 == b2)
