2a96cb1d077c663ea8f9c466a5eab3244c931eb0
Sean Robertson <sdrobert@cs.toronto.edu>
2020-10-26 13:58:18 -0400

    Added some alternate computations + benchmarking

--- poisson_binomial.py
+++ poisson_binomial.py
@@ -1,4 +1,5 @@
 import torch
+import contextlib
 
 
 def R(w, k_max, keep_hist=False, reverse=False):
@@ -21,6 +22,88 @@ def R(w, k_max, keep_hist=False, reverse=False):
         return torch.stack(hist, 0)
     else:
         return torch.cat([R0, Rrest])
+
+
+def R2(w, k_max, keep_hist=False, reverse=False):
+    # in w = (*, T)
+    # out R = (*, k_max + 1) if keep_hist is False, else
+    # R = (*, kmax + 1, T)
+    star = w.shape[:-1]
+    T = w.shape[-1]
+    if reverse:
+        w = w.flip(-1)
+    w = torch.cat([w, torch.zeros_like(w[..., :1])], -1)  # (*, T + 1)
+    w = w.view(-1, 1, T + 1).expand(-1, T + 1, T + 1)  # (N, 1, T + 1, T + 1)
+    w = w.tril(-1)  # (N, T + 1, T + 1)
+    Rk = torch.ones((w.shape[0], T + 1, 1), device=w.device)  # (N, T + 1, 1)
+    if keep_hist:
+        hist = [Rk[..., 0]]  # [(N, T)]
+    else:
+        hist = [Rk[:, -1]]  # [(N, 1)]
+    for k in range(k_max):
+        Rk = torch.bmm(w, Rk)  # (N, T + 1, 1)
+        if keep_hist:
+            hist.append(Rk[..., 0])
+        else:
+            hist.append(Rk[:, -1])
+    if keep_hist:
+        v = torch.stack(hist, 1).view(*(star + (k_max + 1, T + 1)))
+        return v
+    else:
+        return torch.cat(hist, -1).view(*(star + (k_max + 1,)))
+
+
+def R3(w, k_max, keep_hist=False, reverse=False):
+    # R2 but does block multiplication, avoiding about half the total memory usage
+    # in w = (*, T)
+    # out R = (*, k_max + 1) if keep_hist is False, else
+    # R = (*, kmax + 1, T)
+    #
+    #     d           d
+    #     |           |
+    #     v           v
+    # 0 0 0 0 0     0 0 0 0
+    # a 0 0 0 0     a 0 0 0
+    # a a 0 0 0     b b 0 0
+    # b b c 0 0     b b c 0
+    # b b c c 0
+    #
+    # - the rows in B are the same, so we don't waste memory on them
+    star = w.shape[:-1]
+    T = w.shape[-1]
+    w = w.view(-1, T)
+    if reverse:
+        w = w.flip(-1)
+    d = T // 2 + 1
+    B_row = w[:, :d]  # (N, d)
+    A = B_row.view(-1, 1, d).expand(-1, d, d).tril(-1)  # (N, d, d)
+    C = (
+        torch.cat([w[:, d:], torch.zeros((w.shape[0], 1), device=w.device)], -1)
+        .view(-1, 1, T + 1 - d)
+        .expand(-1, T + 1 - d, T + 1 - d)
+        .tril(-1)
+    )  # (N, T + 1 - d, T + 1 - d)
+    Rk_top = torch.ones((w.shape[0], d), device=w.device)  # (N, d)
+    Rk_bot = torch.ones((w.shape[0], T + 1 - d), device=w.device)  # (N, T + 1 - d)
+    if keep_hist:
+        hist = [torch.cat([Rk_top, Rk_bot], -1)]  # [(N, T + 1)]
+    else:
+        hist = [Rk_bot[:, -1]]  # [(N,)]
+    for k in range(k_max):
+        Rk_bot = (B_row * Rk_top).sum(1, keepdim=True).expand(
+            w.shape[0], T + 1 - d
+        ) + torch.bmm(C, Rk_bot.unsqueeze(-1)).squeeze(
+            -1
+        )  # (N, T + 1 - d)
+        Rk_top = torch.bmm(A, Rk_top.unsqueeze(-1)).squeeze(-1)  # (N, d)
+        if keep_hist:
+            hist.append(torch.cat([Rk_top, Rk_bot], -1))  # [(N, T + 1)]
+        else:
+            hist.append(Rk_bot[:, -1])  # [(N,)]
+    if keep_hist:
+        return torch.stack(hist, 1).view(*(star + (k_max + 1, T + 1)))
+    else:
+        return torch.stack(hist, -1).view(*(star + (k_max + 1,)))
 
 
 def lR(logits, k_max, keep_hist=False, reverse=False):
@@ -70,3 +153,148 @@ def lprobs(logits):
     return lR(logits, len(logits)) + torch.nn.functional.logsigmoid(-logits).sum(
         0, keepdim=True
     )
+
+
+def main(args=None):
+    """Benchmark different count approximations"""
+    opts = _parse_args(args)
+
+    if opts.command == "speed":
+        return _speed(opts)
+
+
+def _parse_args(args):
+    import argparse
+
+    parser = argparse.ArgumentParser(description=main.__doc__)
+    parser.add_argument("--seed", type=int, default=None)
+    parser.add_argument("--repeat", type=int, default=100)
+    parser.add_argument("--trials", type=int, default=1000)
+    parser.add_argument("--highs", type=int, default=10)
+    parser.add_argument("--batch", type=int, default=1)
+    parser.add_argument("--device", type=torch.device, default=torch.device("cpu"))
+    parser.add_argument("--method", choices=("R", "R2", "R3", "lR"), default="R")
+
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    speed_parser = subparsers.add_parser("speed")
+    speed_parser.add_argument("--burn-in", type=int, default=100)
+    speed_parser.add_argument("--hist", action="store_true", default=False)
+    speed_parser.add_argument("--reverse", action="store_true", default=False)
+
+    acc_parser = subparsers.add_parser("acc")
+    acc_parser.add_argument("--eps", type=float, default=1e-4)
+
+    return parser.parse_args(args)
+
+
+def _create_weights(opts, repeat_no=0):
+    if opts.seed is not None:
+        torch.manual_seed(opts.seed + repeat_no)
+    w = torch.randn((opts.trials, opts.batch), device=opts.device).exp()
+    if opts.method in {"R2", "R3"}:
+        w = w.transpose(0, 1)
+    if opts.device.type == "cuda":
+        torch.cuda.synchronize()
+    return w
+
+
+@contextlib.contextmanager
+def _cuda_timer():
+    class Timer(object):
+        def __init__(self, start, end):
+            self.start = start
+            self.end = end
+
+        def __call__(self):
+            return self.start.elapsed_time(end)
+
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+    timer = Timer(start, end)
+
+    start.record()
+    try:
+        yield timer
+    finally:
+        end.record()
+        torch.cuda.synchronize()
+
+
+@contextlib.contextmanager
+def _cpu_timer():
+    class Timer(object):
+        def __init__(self):
+            self.start = self.end = None
+
+        def __call__(self):
+            return self.end - self.start
+
+    timer = Timer()
+
+    import time
+
+    start = time.time() * 1000.0
+    try:
+        yield timer
+    finally:
+        end = time.time() * 1000.0
+        timer.start, timer.end = start, end
+
+
+def _speed(opts):
+    import numpy as np
+
+    cum_times = []
+
+    if opts.method == "R":
+        _R = R
+    elif opts.method == "lR":
+        _R = lR
+    elif opts.method == "R2":
+        _R = R2
+    elif opts.method == "R3":
+        _R = R3
+
+    if opts.device.type == "cuda":
+        timeit = _cuda_timer
+    else:
+        timeit = _cpu_timer
+
+    w = _create_weights(opts)
+
+    for _ in range(opts.burn_in):
+        _R(w, opts.highs, opts.hist, opts.reverse)
+
+    for repeat_no in range(opts.repeat):
+
+        with timeit() as timer:
+            _R(w, opts.highs, opts.hist, opts.reverse)
+
+        cum_times.append(timer())
+
+    cum_times = np.array(cum_times)
+
+    print(
+        "batch={},burn_in={},command={},device={},highs={},hist={},method={},"
+        "repeat={},reverse={},seed={},trials={},mu(ms)={:.02f},std(ms)={:.02f}"
+        "".format(
+            opts.batch,
+            opts.burn_in,
+            opts.command,
+            opts.device.type,
+            opts.highs,
+            opts.hist,
+            opts.method,
+            opts.repeat,
+            opts.reverse,
+            opts.seed,
+            opts.trials,
+            cum_times.mean(),
+            cum_times.std(),
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
--- test_poisson_binomial.py
+++ test_poisson_binomial.py
@@ -3,16 +3,48 @@ import torch
 import pytest
 
 
+@pytest.fixture(scope="module", params=["R", "R2", "R3"])
+def R(request):
+    if request.param == "R":
+        return poisson_binomial.R
+    elif request.param == "R2":
+
+        def _R(w, *args, **kwargs):
+            T, star = w.shape[0], w.shape[1:]
+            w = w.reshape(T, -1).transpose(0, 1)
+            v = poisson_binomial.R2(w, *args, **kwargs)
+            v = v.transpose(0, -1)
+            v = v.reshape(*(v.shape[:-1] + star))
+            return v
+
+        return _R
+    elif request.param == "R3":
+
+        def _R(w, *args, **kwargs):
+            T, star = w.shape[0], w.shape[1:]
+            w = w.reshape(T, -1).transpose(0, 1)
+            v = poisson_binomial.R3(w, *args, **kwargs)
+            v = v.transpose(0, -1)
+            v = v.reshape(*(v.shape[:-1] + star))
+            return v
+
+        return _R
+
+
 # this isn't numerically stable
 def naive_R(w, k_max):
     # in w = (S,)
-    S, = w.shape
+    (S,) = w.shape
     assert 0 <= k_max
     R = torch.ones(1, device=w.device)
     T = (
-        ((-w).unsqueeze(0) ** torch.arange(1, k_max + 2).unsqueeze(1))
-        / w.unsqueeze(0)
-    ).sum(1)[1:].flip(0)  # [-1^{k+1} T(k), -1^k T(k - 1), ..., T(1)]
+        (
+            ((-w).unsqueeze(0) ** torch.arange(1, k_max + 2).unsqueeze(1))
+            / w.unsqueeze(0)
+        )
+        .sum(1)[1:]
+        .flip(0)
+    )  # [-1^{k+1} T(k), -1^k T(k - 1), ..., T(1)]
     for k in range(1, k_max + 1):
         if k <= S:
             R_new = (R * T[-k:]).sum(0, keepdim=True) / k
@@ -27,91 +59,109 @@ def test_naive_R():
     w = torch.rand(4)
     w.requires_grad_(True)
     w_1, w_2, w_3, w_4 = w.unsqueeze(-1)
-    R_exp = torch.cat([
-        torch.tensor([1.]),
-        w_1 + w_2 + w_3 + w_4,
-        w_1 * w_2 + w_1 * w_3 + w_1 * w_4 + w_2 * w_3 + w_2 * w_4 + w_3 * w_4,
-        w_1 * w_2 * w_3 + w_1 * w_2 * w_4 + w_1 * w_3 * w_4 + w_2 * w_3 * w_4,
-        w_1 * w_2 * w_3 * w_4,
-        torch.tensor([0., 0.]),
-    ], 0)
-    g_exp, = torch.autograd.grad(R_exp, w, torch.ones_like(R_exp))
+    R_exp = torch.cat(
+        [
+            torch.tensor([1.0]),
+            w_1 + w_2 + w_3 + w_4,
+            w_1 * w_2 + w_1 * w_3 + w_1 * w_4 + w_2 * w_3 + w_2 * w_4 + w_3 * w_4,
+            w_1 * w_2 * w_3 + w_1 * w_2 * w_4 + w_1 * w_3 * w_4 + w_2 * w_3 * w_4,
+            w_1 * w_2 * w_3 * w_4,
+            torch.tensor([0.0, 0.0]),
+        ],
+        0,
+    )
+    (g_exp,) = torch.autograd.grad(R_exp, w, torch.ones_like(R_exp))
     w.requires_grad_(True)
     R_act = naive_R(w, 6)
     assert R_exp.shape == R_act.shape
     assert torch.allclose(R_exp, R_act, atol=1e-5)
-    g_act, = torch.autograd.grad(R_act, w, torch.ones_like(R_act))
+    (g_act,) = torch.autograd.grad(R_act, w, torch.ones_like(R_act))
     assert torch.allclose(g_exp, g_act, atol=1e-5)
 
 
-@pytest.mark.parametrize('reverse', [True, False])
-def test_shift_R(reverse):
+@pytest.mark.parametrize("reverse", [True, False])
+def test_R(reverse, R):
     torch.manual_seed(2154)
     w = torch.rand(50)
     R_exp = naive_R(w, 10)
-    R_act = poisson_binomial.R(w, 10, reverse=reverse)
+    R_act = R(w, 10, reverse=reverse)
     assert torch.allclose(R_exp, R_act)
-    R_act = poisson_binomial.R(w.unsqueeze(-1).expand(-1, 30), 10)
-    assert torch.allclose(R_exp, R_act[..., 11])
+    R_act = R(w.unsqueeze(-1).expand(-1, 30), 10)
+    assert torch.allclose(R_exp.unsqueeze(-1).expand_as(R_act), R_act), (
+        R_exp[:5],
+        R_act[:3, :5],
+    )
 
 
-@pytest.mark.parametrize('reverse', [True, False])
-def test_shift_R_history(reverse):
+@pytest.mark.parametrize("reverse", [True, False])
+def test_R_history(reverse, R):
     torch.manual_seed(7420)
     w = torch.rand(4)
     w1, w2, w3, w4 = w.tolist()
     if reverse:
-        Rhist_exp = torch.tensor([
-            [1, 1, 1, 1, 1],
-            [0, w4, w4 + w3, w4 + w3 + w2, w4 + w3 + w2 + w1],
+        Rhist_exp = torch.tensor(
             [
-                0, 0, w4 * w3, w4 * w3 + w4 * w2 + w3 * w2, w4 * w3 +
-                w4 * w2 + w4 * w1 + w3 * w2 + w3 * w1 + w2 * w1
+                [1, 1, 1, 1, 1],
+                [0, w4, w4 + w3, w4 + w3 + w2, w4 + w3 + w2 + w1],
+                [
+                    0,
+                    0,
+                    w4 * w3,
+                    w4 * w3 + w4 * w2 + w3 * w2,
+                    w4 * w3 + w4 * w2 + w4 * w1 + w3 * w2 + w3 * w1 + w2 * w1,
+                ],
             ]
-        ]).T
+        ).T
     else:
-        Rhist_exp = torch.tensor([
-            [1, 1, 1, 1, 1],
-            [0, w1, w1 + w2, w1 + w2 + w3, w1 + w2 + w3 + w4],
+        Rhist_exp = torch.tensor(
             [
-                0, 0, w1 * w2, w1 * w2 + w1 * w3 + w2 * w3, w1 * w2 +
-                w1 * w3 + w1 * w4 + w2 * w3 + w2 * w4 + w3 * w4
+                [1, 1, 1, 1, 1],
+                [0, w1, w1 + w2, w1 + w2 + w3, w1 + w2 + w3 + w4],
+                [
+                    0,
+                    0,
+                    w1 * w2,
+                    w1 * w2 + w1 * w3 + w2 * w3,
+                    w1 * w2 + w1 * w3 + w1 * w4 + w2 * w3 + w2 * w4 + w3 * w4,
+                ],
             ]
-        ]).T
-    Rhist_act = poisson_binomial.R(
-        w.unsqueeze(-1).expand(-1, 10), 2, True, reverse)
-    assert torch.allclose(Rhist_exp, Rhist_act[..., 4])
+        ).T
+    Rhist_act = R(w.unsqueeze(-1).expand(-1, 2), 2, True, reverse)
+    assert torch.allclose(Rhist_exp, Rhist_act[..., 1])
 
 
-def test_R_properties():
+def test_R_properties(R):
     torch.manual_seed(4201)
     T, N, k = 30, 4, 5
     w = torch.rand(T, N)
 
     # sum_j w_j R(k-1, C\{j}) = k R(k, C)
-    w_sub_j = w.unsqueeze(0).masked_fill(
-        torch.eye(T, dtype=bool).unsqueeze(-1), 0.).transpose(0, 1)
+    w_sub_j = (
+        w.unsqueeze(0)
+        .masked_fill(torch.eye(T, dtype=bool).unsqueeze(-1), 0.0)
+        .transpose(0, 1)
+    )
     assert torch.allclose(w_sub_j.sum(1) / (T - 1), w)
-    R_sub_j = poisson_binomial.R(w_sub_j, k)
+    R_sub_j = R(w_sub_j, k)
     R_k_act = (R_sub_j[-2] * w).sum(0) / k
-    R = poisson_binomial.R(w, k)
-    R_k_exp = R[-1]
+    Rk = R(w, k)
+    R_k_exp = Rk[-1]
     assert torch.allclose(R_k_act, R_k_exp)
 
     # sum_j R(k - 1, C\{j}) = (T - k) R(k, C)
-    assert torch.allclose(R_sub_j[-1].sum(0), (T - k) * R[-1])
+    assert torch.allclose(R_sub_j[-1].sum(0), (T - k) * Rk[-1])
 
     # sum_i R(i, C) R(k - i, C^c) = R(k, C)
-    w_left = w[:T // 2]
-    w_right = w[T // 2:]
-    R_left = poisson_binomial.R(w_left, k)
-    R_right = poisson_binomial.R(w_right, k)
+    w_left = w[: T // 2]
+    w_right = w[T // 2 :]
+    R_left = R(w_left, k)
+    R_right = R(w_right, k)
     R_k_act = (R_left * R_right.flip(0)).sum(0)
     assert torch.allclose(R_k_act, R_k_exp)
 
 
-@pytest.mark.parametrize('reverse', [True, False])
-def test_shift_R_zero_weights(reverse):
+@pytest.mark.parametrize("reverse", [True, False])
+def test_shift_R_zero_weights(reverse, R):
     torch.manual_seed(1702)
     T, N, k_max = 30, 10, 4
     w_lens = torch.randint(k_max, T + 1, (N,))
@@ -120,19 +170,19 @@ def test_shift_R_zero_weights(reverse):
     g_exp = []
     for w_len in w_lens.tolist():
         w_n = torch.rand(w_len)
-        w_n[0] = 0.
+        w_n[0] = 0.0
         w_n.requires_grad_(True)
-        R_n = poisson_binomial.R(w_n, k_max, reverse=reverse)
-        g_n, = torch.autograd.grad(R_n, w_n, torch.ones_like(R_n))
+        R_n = R(w_n, k_max, reverse=reverse)
+        (g_n,) = torch.autograd.grad(R_n, w_n, torch.ones_like(R_n))
         R_exp.append(R_n)
         g_exp.append(g_n)
         w.append(w_n)
     w = torch.nn.utils.rnn.pad_sequence(w)
     R_exp = torch.stack(R_exp, dim=-1)
     w.requires_grad_(True)
-    R_act = poisson_binomial.R(w, k_max, reverse=reverse)
+    R_act = R(w, k_max, reverse=reverse)
     assert torch.allclose(R_exp, R_act)
-    g_act, = torch.autograd.grad(R_act, w, torch.ones_like(R_act))
+    (g_act,) = torch.autograd.grad(R_act, w, torch.ones_like(R_act))
     # zero weights can still have a gradient, but that shouldn't affect the
     # gradient of other weights
     for w_len, g_exp_n, g_act_n in zip(w_lens, g_exp, g_act.T):
@@ -140,8 +190,8 @@ def test_shift_R_zero_weights(reverse):
         assert torch.allclose(g_exp_n[:w_len], g_act_n[:w_len])
 
 
-@pytest.mark.parametrize('keep_hist', [True, False])
-@pytest.mark.parametrize('reverse', [True, False])
+@pytest.mark.parametrize("keep_hist", [True, False])
+@pytest.mark.parametrize("reverse", [True, False])
 def test_shift_log_R(keep_hist, reverse):
     torch.manual_seed(198236)
     w = torch.rand(50, 4, 30, 10)
@@ -150,7 +200,7 @@ def test_shift_log_R(keep_hist, reverse):
     assert torch.allclose(R_exp, R_act)
 
 
-@pytest.mark.parametrize('reverse', [True, False])
+@pytest.mark.parametrize("reverse", [True, False])
 def test_shift_log_R_inf_logits(reverse):
     torch.manual_seed(3291)
     T, N, k_max = 53, 5, 3
@@ -160,26 +210,25 @@ def test_shift_log_R_inf_logits(reverse):
     g_exp = []
     for logits_len in logits_lens.tolist():
         logits_n = torch.randn(logits_len)
-        logits_n[0] = -float('inf')
+        logits_n[0] = -float("inf")
         logits_n.requires_grad_(True)
         R_n = poisson_binomial.lR(logits_n, k_max, reverse=reverse)
-        g_n, = torch.autograd.grad(R_n, logits_n, torch.ones_like(R_n))
+        (g_n,) = torch.autograd.grad(R_n, logits_n, torch.ones_like(R_n))
         R_exp.append(R_n)
         g_exp.append(g_n)
         logits.append(logits_n)
-    logits = torch.nn.utils.rnn.pad_sequence(
-        logits, padding_value=-float('inf'))
+    logits = torch.nn.utils.rnn.pad_sequence(logits, padding_value=-float("inf"))
     R_exp = torch.stack(R_exp, dim=-1)
     logits.requires_grad_(True)
     R_act = poisson_binomial.lR(logits, k_max, reverse=reverse)
     assert torch.allclose(R_exp, R_act)
-    g_act, = torch.autograd.grad(R_act, logits, torch.ones_like(R_act))
+    (g_act,) = torch.autograd.grad(R_act, logits, torch.ones_like(R_act))
     for logits_len, g_exp_n, g_act_n in zip(logits_lens, g_exp, g_act.T):
         assert not torch.any(torch.isnan(g_exp_n))
         assert torch.allclose(g_exp_n[:logits_len], g_act_n[:logits_len])
         # the gradient isn't really defined for -inf in the log-space, but it
         # works out to be the non-threatening zero here
-        assert torch.all(g_act_n[logits_len:] == 0.)
+        assert torch.all(g_act_n[logits_len:] == 0.0)
 
 
 def test_poisson_binomial_probs():
@@ -188,11 +237,10 @@ def test_poisson_binomial_probs():
     bern_p = torch.rand(T)
     bern_p /= torch.rand(T).sum()
     w = bern_p / (1 - bern_p)
-    s = torch.distributions.Bernoulli(
-        probs=bern_p.unsqueeze(-1).expand(T, N)).sample()
+    s = torch.distributions.Bernoulli(probs=bern_p.unsqueeze(-1).expand(T, N)).sample()
     counts = s.sum(0)
     mc_probs = (torch.arange(T + 1).unsqueeze(-1) == counts).float().mean(-1)
-    assert torch.isclose(mc_probs.sum(), torch.tensor(1.))
+    assert torch.isclose(mc_probs.sum(), torch.tensor(1.0))
     pred_probs = poisson_binomial.probs(w)
     assert torch.allclose(pred_probs, mc_probs, atol=1e-3)
 
@@ -201,11 +249,10 @@ def test_poisson_binomial_log_probs():
     torch.manual_seed(24229027)
     T, N = 30, 1000000
     logits = torch.randn(T)
-    s = torch.distributions.Bernoulli(
-        logits=logits.unsqueeze(-1).expand(T, N)).sample()
+    s = torch.distributions.Bernoulli(logits=logits.unsqueeze(-1).expand(T, N)).sample()
     counts = s.sum(0)
     mc_probs = (torch.arange(T + 1).unsqueeze(-1) == counts).float().mean(-1)
-    assert torch.isclose(mc_probs.sum(), torch.tensor(1.))
+    assert torch.isclose(mc_probs.sum(), torch.tensor(1.0))
     pred_probs = poisson_binomial.lprobs(logits).exp()
-    assert torch.isclose(pred_probs.sum(), torch.tensor(1.))
+    assert torch.isclose(pred_probs.sum(), torch.tensor(1.0))
     assert torch.allclose(pred_probs, mc_probs, atol=1e-3)
