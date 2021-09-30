dca2c8830377153afc94f947fe689c09198f89fc
Sean Robertson <sdrobert@cs.toronto.edu>
2021-09-30 13:06:44 -0400

    Added sswor

--- distributions.py
+++ distributions.py
@@ -1,16 +1,17 @@
 """Distributions, in particular the Conditional Bernoulli"""
 
+import math
 from forward_backward import (
-    EPS_INF,
     R_forward,
     extract_relevant_odds_forward,
     log_R_forward,
 )
+import config
 from typing import Optional, Union
 import torch
 
 import torch.distributions.constraints as constraints
-from torch.distributions.utils import lazy_property
+from torch.distributions.utils import lazy_property, logits_to_probs
 
 
 class PoissonBinomial(torch.distributions.Distribution):
@@ -204,6 +205,27 @@ def _extended_conditional_bernoulli_k_eq_1(
     return b[:, :tmax]
 
 
+class ConditionalBernoulliConstraint(constraints.Constraint):
+    is_discrete = True
+    event_dim = 1
+
+    def __init__(
+        self, total_count: torch.Tensor, given_count: torch.Tensor, tmax: int
+    ) -> None:
+        self.given_count = given_count
+        self.total_count_mask = total_count.unsqueeze(-1) <= torch.arange(
+            tmax, device=total_count.device
+        )
+        super().__init__()
+
+    def check(self, value: torch.Tensor) -> torch.Tensor:
+        is_bool = ((value == 0) | (value == 1)).all(-1)
+        isnt_gte_tc = (self.total_count_mask.expand_as(value) * value).sum(-1) == 0
+        value_sum = value.sum(-1)
+        matches_count = value_sum == self.given_count.expand_as(value_sum)
+        return is_bool & isnt_gte_tc & matches_count
+
+
 class ConditionalBernoulli(torch.distributions.ExponentialFamily):
 
     arg_constraints = {
@@ -213,26 +235,6 @@ class ConditionalBernoulli(torch.distributions.ExponentialFamily):
         "logits": constraints.real,
     }
     _mean_carrier_measure = 0
-
-    class SupportConstraint(constraints.Constraint):
-        is_discrete = True
-        event_dim = 1
-
-        def __init__(
-            self, total_count: torch.Tensor, given_count: torch.Tensor, tmax: int
-        ) -> None:
-            self.given_count = given_count
-            self.total_count_mask = total_count.unsqueeze(-1) >= torch.arange(
-                tmax, device=total_count.device
-            )
-            super().__init__()
-
-        def check(self, value: torch.Tensor) -> torch.Tensor:
-            is_bool = ((value == 0) | (value == 1)).all(-1)
-            isnt_gte_tc = (self.total_count_mask.expand_as(value) * value).sum(-1) != 0
-            value_sum = value.sum(-1)
-            matches_count = value_sum == self.given_count.expand_as(value_sum)
-            return is_bool & isnt_gte_tc & matches_count
 
     def __init__(
         self,
@@ -279,7 +281,7 @@ class ConditionalBernoulli(torch.distributions.ExponentialFamily):
 
     @constraints.dependent_property
     def support(self) -> torch.Tensor:
-        return self.SupportConstraint(
+        return ConditionalBernoulliConstraint(
             self.total_count, self.given_count, self._event_shape[0]
         )
 
@@ -357,11 +359,13 @@ class ConditionalBernoulli(torch.distributions.ExponentialFamily):
 
     @property
     def mean(self):
-        return self.probs.sum(-1)
+        # inclusion probabilities
+        raise NotImplementedError
 
     @property
     def variance(self):
-        return (self.probs * (1.0 - self.probs)).sum(-1)
+        # ah geez whatever
+        raise NotImplementedError
 
     @property
     def param_shape(self):
@@ -424,9 +428,133 @@ class ConditionalBernoulli(torch.distributions.ExponentialFamily):
     def log_prob(self, value: torch.Tensor) -> torch.Tensor:
         if self._validate_args:
             self._validate_sample(value)
-        num = (value * self.logits.clamp_min(EPS_INF).expand_as(value)).sum(-1)
+        num = (value * self.logits.clamp_min(config.EPS_INF).expand_as(value)).sum(-1)
         denom = self.log_partition.expand_as(num)
         return num - denom
+
+
+@torch.jit.script
+@torch.no_grad()
+def simple_sampling_without_replacement(
+    tmax: torch.Tensor, lmax: torch.Tensor, tmax_max: Optional[int] = None
+) -> torch.Tensor:
+    if tmax_max is None:
+        tmax_max = int(tmax.max().item())
+    tmax, lmax = torch.broadcast_tensors(tmax, lmax)
+    b = torch.empty(torch.Size([tmax_max]) + tmax.shape)
+    remainder_ell = lmax
+    remainder_t = tmax
+    for t in range(tmax_max):
+        p = remainder_ell / remainder_t
+        b_t = torch.bernoulli(p)
+        b[t] = b_t
+        remainder_ell = remainder_ell - b_t
+        remainder_t = (remainder_t - 1).clamp_min_(1)
+    return b.movedim(0, -1)
+
+
+class SimpleSamplingWithoutReplacement(torch.distributions.ExponentialFamily):
+
+    arg_constraints = {
+        "total_count": constraints.nonnegative_integer,
+        "given_count": constraints.nonnegative_integer,
+    }
+    _mean_carrier_measure = 0
+
+    def __init__(
+        self,
+        given_count: Union[int, torch.Tensor],
+        total_count: Union[int, torch.Tensor],
+        out_size: Optional[int] = None,
+        validate_args=None,
+    ):
+        given_count = torch.as_tensor(given_count)
+        total_count = torch.as_tensor(total_count)
+        if out_size is None:
+            out_size = total_count.max()
+        given_count, total_count = torch.broadcast_tensors(given_count, total_count)
+        batch_shape = given_count.size()
+        event_shape = torch.Size([out_size])
+        self.total_count, self.given_count = total_count, given_count
+        super(SimpleSamplingWithoutReplacement, self).__init__(
+            batch_shape, event_shape, validate_args
+        )
+
+    @constraints.dependent_property
+    def support(self) -> torch.Tensor:
+        return ConditionalBernoulliConstraint(
+            self.total_count, self.given_count, self.event_shape[0]
+        )
+
+    @lazy_property
+    def log_partition(self) -> torch.Tensor:
+        # log total_count choose given_count
+        log_factorial = (
+            torch.arange(
+                1,
+                self.event_shape[0] + 1,
+                device=self.total_count.device,
+                dtype=torch.float,
+            )
+            .log()
+            .cumsum(0)
+        )
+        t_idx = (self.total_count.long() - 1).clamp_min(0)
+        g_idx = (self.given_count.long() - 1).clamp_min(0)
+        tmg_idx = (self.total_count.long() - self.given_count.long() - 1).clamp_min(0)
+        return log_factorial[t_idx] - log_factorial[g_idx] - log_factorial[tmg_idx]
+
+    @property
+    def mean(self):
+        len_mask = self.total_count.unsqueeze(-1) <= torch.arange(self.event_shape[0])
+        return (
+            (self.given_count / self.total_count.clamp_min(1.0))
+            .unsqueeze(-1)
+            .expand(self.batch_shape + self.event_shape)
+        ).masked_fill(len_mask, 0.0)
+
+    @property
+    def variance(self):
+        return self.mean * (1 - self.mean)
+
+    @property
+    def _natural_params(self):
+        return self.zeros(self.batch_shape + self.event_shape)
+
+    def _log_normalizer(self, logits):
+        assert torch.allclose(logits, torch.tensor(0))
+        return self.log_partition
+
+    def expand(self, batch_shape, _instance=None):
+        new = self._get_checked_instance(SimpleSamplingWithoutReplacement, _instance)
+        batch_shape = list(batch_shape)
+        new.given_count = self.given_count.expand(batch_shape)
+        new.total_count = self.total_count.expand(batch_shape)
+
+        if "log_partition" in self.__dict__:
+            new.log_partition = self.log_partition.expand(batch_shape)
+
+        super(SimpleSamplingWithoutReplacement, new).__init__(
+            torch.Size(batch_shape), self.event_shape, validate_args=False
+        )
+        new._validate_args = self._validate_args
+        return new
+
+    def sample(self, sample_shape=torch.Size([])):
+        sample_shape = torch.Size(sample_shape)
+        shape = self._extended_shape(sample_shape)
+        with torch.no_grad():
+            total_count = self.total_count.expand(shape[:-1])
+            given_count = self.given_count.expand(shape[:-1])
+            b = simple_sampling_without_replacement(
+                total_count, given_count, self.event_shape[0]
+            )
+        return b
+
+    def log_prob(self, value):
+        if self._validate_args:
+            self._validate_sample(value)
+        return (-self.log_partition).expand(value.shape[:-1])
 
 
 # TESTS
@@ -480,7 +608,7 @@ def test_conditional_bernoulli():
     b = conditional_bernoulli.sample([mmax])
     assert b.shape == torch.Size([mmax, nmax, tmax])
     assert (b.sum(-1) == 2).all()
-    assert (b.max(-1)[0] == 1).all()
+    assert ((b == 0.0) | (b == 1.0)).all()
     inclusions_act = b.mean(0)
     assert torch.allclose(inclusions_exp, inclusions_act, atol=1e-2)
 
@@ -507,6 +635,35 @@ def test_conditional_bernoulli():
     assert (b.sum(-1) == given_count.unsqueeze(0)).all()
     assert ((b * total_count_mask).sum(-1) == given_count.unsqueeze(0)).all()
     assert (b.max(-1)[0] <= 1).all()
+
+
+def test_simple_sampling_without_replacement():
+    torch.manual_seed(3)
+    tmax_max, nmax, mmax = 16, 8, 2 ** 15
+    tmax = torch.randint(tmax_max + 1, size=(nmax,), dtype=torch.float)
+    lmax = (torch.rand(nmax) * (tmax + 1)).floor_()
+
+    sswor = SimpleSamplingWithoutReplacement(lmax, tmax, tmax_max, True)
+    b = sswor.sample([mmax])
+    assert ((b == 0.0) | (b == 1.0)).all()
+    assert (b.sum(-1) == lmax).all()
+    tmax_mask = tmax.unsqueeze(1) > torch.arange(tmax_max)
+    b = b * tmax_mask
+    assert (b.sum(-1) == lmax).all()
+    assert torch.allclose(b.mean(0), sswor.mean, atol=1e-2)
+
+    lp_exp = []
+    for n in range(nmax):
+        tmax_n, lmax_n = int(tmax[n].item()), int(lmax[n].item())
+        lp_exp.append(
+            math.log(
+                (math.factorial(tmax_n - lmax_n) * math.factorial(lmax_n))
+                / math.factorial(tmax_n)
+            )
+        )
+    lp_exp = torch.tensor(lp_exp).expand(mmax, nmax)
+    lp_act = sswor.log_prob(b)
+    assert torch.allclose(lp_exp, lp_act)
 
 
 # XXX(sdrobert): The following did not work. I've had difficulty with algorithms based
