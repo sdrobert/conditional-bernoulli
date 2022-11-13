5422dd0e232a390ff1e408ba8b3a2074752003db
sdrobert <sdrobert@cs.toronto.edu>
2022-11-13 12:37:55 -0500

    Up

--- timit.sh
+++ timit.sh
@@ -45,7 +45,7 @@ EOF
 
 # constants
 # ALL_MODELS=( $(find conf/proto -mindepth 1 -type d -exec basename {} \;) )
-ALL_MODELS=( deeprnn-uni )
+ALL_MODELS=( lcjs )
 ALL_DEPENDENCIES=( full indep partial )
 ALL_ESTIMATORS=( direct marginal cb srswor ais-c ais-g sf-biased sf-is ctc )
 ALL_LMS=( lm-rnn lm-embedding nolm )
@@ -81,7 +81,7 @@ dependencies=( "${ALL_DEPENDENCIES[@]}" )
 estimators=( "${ALL_ESTIMATORS[@]}" )
 lms=( "${ALL_LMS[@]}" )
 regimes=( "${ALL_REGIMES[@]}" )
-beam_widths=( 1 2 4 8 16 32 64 128 )
+beam_widths=( 1 2 4 8 )
 # beam_widths=( 4 )
 only=0
 
